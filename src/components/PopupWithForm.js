63f19ed7b3919d941cdbc9263def62ff65b38b68
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-10-31 13:22:35 +0400

    feat: added function in component PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,5 +1,17 @@
 import '../index.css';
 
-export default function PopupWithForm() {
-  return;
+export default function PopupWithForm({ title, name, children }) {
+  return (
+    <div className={`popup popup_type_${name}`}>
+      <form className="popup__container popup__container_type_form" name="container" novalidate>
+        <button className="popup__btn popup__btn_action_close" type="button"></button>
+        <h2 className="popup__title">{title}</h2>
+        {/* {children} */}
+        <button className="popup__btn popup__btn_action_submit" type="submit">
+          <span className="popup__btn-label popup__btn-label_type_static popup__btn-label_visible">Сохранить</span>
+          <span className="popup__btn-label popup__btn-label_type_doing">Сохранение...</span>
+        </button>
+      </form>
+    </div>
+  );
 }
