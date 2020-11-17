43ca0a005fd848eb8f7e87df4f8b5558447d21cc
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-17 19:09:45 +0400

    feat: added CurrentUserContext

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import {
   propsPopupWithConfirmForm,
 } from '../utils/constants.js';
 import { apiObject } from '../utils/api.js';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
@@ -53,113 +54,115 @@ function App() {
   };
 
   return (
-    <div className="page">
-      <Header logo={importedLogo} />
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditAvatar={handleEditAvatarClick}
-        onCardClick={handleCardClick}
-      />
-      <Footer />
-
-      {/* Popup for open form to add new card */}
-      <PopupWithForm
-        title={propsPopupWithAddForm.title}
-        name={propsPopupWithAddForm.name}
-        submitStates={propsPopupWithAddForm.submitStates}
-        isOpen={isAddPlacePopupOpen}
-        isLoad={false}
-        onClose={closeAllPopups}
-      >
-        <input
-          type="text"
-          className="popup__input popup__input_type_name"
-          name="name"
-          id="add-name-input"
-          placeholder="Название"
-          minLength="2"
-          maxLength="30"
-          required
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header logo={importedLogo} />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
         />
-        <span className="popup__error" id="add-name-input-error"></span>
-        <input
-          type="url"
-          className="popup__input popup__input_type_link"
-          name="link"
-          id="link-input-add"
-          placeholder="Ссылка на картинку"
-          required
-        />
-        <span className="popup__error" id="link-input-add-error"></span>
-      </PopupWithForm>
+        <Footer />
 
-      {/* Popup for open form to edit profile */}
-      <PopupWithForm
-        title={propsPopupWithEditForm.title}
-        name={propsPopupWithEditForm.name}
-        submitStates={propsPopupWithEditForm.submitStates}
-        isOpen={isEditProfilePopupOpen}
-        isLoad={false}
-        onClose={closeAllPopups}
-      >
-        <input
-          type="text"
-          className="popup__input popup__input_type_name"
-          name="name"
-          id="edit-name-input"
-          placeholder="Заголовок профиля"
-          minLength="2"
-          maxLength="40"
-          required
-        />
-        <span className="popup__error" id="edit-name-input-error"></span>
-        <input
-          type="text"
-          className="popup__input popup__input_type_about"
-          name="about"
-          id="about-input"
-          placeholder="Описание профиля"
-          minLength="2"
-          maxLength="200"
-          required
-        />
-        <span className="popup__error" id="about-input-error"></span>
-      </PopupWithForm>
+        {/* Popup for open form to add new card */}
+        <PopupWithForm
+          title={propsPopupWithAddForm.title}
+          name={propsPopupWithAddForm.name}
+          submitStates={propsPopupWithAddForm.submitStates}
+          isOpen={isAddPlacePopupOpen}
+          isLoad={false}
+          onClose={closeAllPopups}
+        >
+          <input
+            type="text"
+            className="popup__input popup__input_type_name"
+            name="name"
+            id="add-name-input"
+            placeholder="Название"
+            minLength="2"
+            maxLength="30"
+            required
+          />
+          <span className="popup__error" id="add-name-input-error"></span>
+          <input
+            type="url"
+            className="popup__input popup__input_type_link"
+            name="link"
+            id="link-input-add"
+            placeholder="Ссылка на картинку"
+            required
+          />
+          <span className="popup__error" id="link-input-add-error"></span>
+        </PopupWithForm>
 
-      {/* Popup for open form to edit avatar */}
-      <PopupWithForm
-        title={propsPopupWithEditAvatarForm.title}
-        name={propsPopupWithEditAvatarForm.name}
-        submitStates={propsPopupWithEditAvatarForm.submitStates}
-        isOpen={isEditAvatarPopupOpen}
-        isLoad={false}
-        onClose={closeAllPopups}
-      >
-        <input
-          type="url"
-          className="popup__input popup__input_type_link"
-          name="link"
-          id="link-input-edit"
-          placeholder="Ссылка на картинку"
-          required
+        {/* Popup for open form to edit profile */}
+        <PopupWithForm
+          title={propsPopupWithEditForm.title}
+          name={propsPopupWithEditForm.name}
+          submitStates={propsPopupWithEditForm.submitStates}
+          isOpen={isEditProfilePopupOpen}
+          isLoad={false}
+          onClose={closeAllPopups}
+        >
+          <input
+            type="text"
+            className="popup__input popup__input_type_name"
+            name="name"
+            id="edit-name-input"
+            placeholder="Заголовок профиля"
+            minLength="2"
+            maxLength="40"
+            required
+          />
+          <span className="popup__error" id="edit-name-input-error"></span>
+          <input
+            type="text"
+            className="popup__input popup__input_type_about"
+            name="about"
+            id="about-input"
+            placeholder="Описание профиля"
+            minLength="2"
+            maxLength="200"
+            required
+          />
+          <span className="popup__error" id="about-input-error"></span>
+        </PopupWithForm>
+
+        {/* Popup for open form to edit avatar */}
+        <PopupWithForm
+          title={propsPopupWithEditAvatarForm.title}
+          name={propsPopupWithEditAvatarForm.name}
+          submitStates={propsPopupWithEditAvatarForm.submitStates}
+          isOpen={isEditAvatarPopupOpen}
+          isLoad={false}
+          onClose={closeAllPopups}
+        >
+          <input
+            type="url"
+            className="popup__input popup__input_type_link"
+            name="link"
+            id="link-input-edit"
+            placeholder="Ссылка на картинку"
+            required
+          />
+          <span className="popup__error" id="link-input-edit-error"></span>
+        </PopupWithForm>
+
+        {/* Popup for open form to confirm action */}
+        <PopupWithForm
+          title={propsPopupWithConfirmForm.title}
+          name={propsPopupWithConfirmForm.name}
+          submitStates={propsPopupWithConfirmForm.submitStates}
+          isOpen={isConfirmOpen}
+          isLoad={false}
+          onClose={closeAllPopups}
         />
-        <span className="popup__error" id="link-input-edit-error"></span>
-      </PopupWithForm>
 
-      {/* Popup for open form to confirm action */}
-      <PopupWithForm
-        title={propsPopupWithConfirmForm.title}
-        name={propsPopupWithConfirmForm.name}
-        submitStates={propsPopupWithConfirmForm.submitStates}
-        isOpen={isConfirmOpen}
-        isLoad={false}
-        onClose={closeAllPopups}
-      />
-
-      {/* Popup which shows full image*/}
-      <ImagePopup card={selectedCard} isOpen={isImagePopupOpen} onClose={closeAllPopups} />
-    </div>
+        {/* Popup which shows full image*/}
+        <ImagePopup card={selectedCard} isOpen={isImagePopupOpen} onClose={closeAllPopups} />
+      </div>
+    </CurrentUserContext>
   );
 }
 
