d5936da9301937d71a2cdca88fd6d806cf73c85e
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-11-17 20:07:57 +0400

    ref: refactoring code in card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import { useContext } from 'react';
+import cn from 'classnames';
 
 export default function Card({ card, onCardClick }) {
   const currentUser = useContext(CurrentUserContext);
@@ -10,17 +11,16 @@ export default function Card({ card, onCardClick }) {
 
   return (
     <li className="card">
-      {currentUser._id === card.owner._id ? (
+      {currentUser._id === card.owner._id && (
         <button className="card__btn card__btn_action_trush" type="button"></button>
-      ) : (
-        ''
       )}
+
       <img src={card.link} alt={card.name} className="card__image" onClick={handleClick} />
       <div className="card__footer">
         <h3 className="card__caption">{card.name}</h3>
         <div className="like card__like">
           <button
-            className={`like__btn ${card.likes.some((item) => item._id === currentUser._id) ? 'like__btn_active' : ''}`}
+            className={cn('like__btn', { like__btn_active: card.likes.some((item) => item._id === currentUser._id) })}
             type="button"
           ></button>
           <p className="like__count" title={card.likes.reduce((res, item) => (res += `${item.name}\n`), '')}>
